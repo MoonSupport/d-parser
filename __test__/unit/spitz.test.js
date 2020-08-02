373e23e5b627701caefe2e5b5ddb20beaace85a9
MoonSupport <jiwon3346@naver.com>
2020-08-02 22:29:50 +0900

    add test

--- __test__/unit/spitz.test.js
+++ __test__/unit/spitz.test.js
@@ -1,7 +1,23 @@
 var assert = require('assert')
 var { Type } = require('../../build/type')
-var { processTextNode } = require('../../build/process.js')
+var {
+  processTextNode,
+  processElementNode,
+  initRootStack,
+} = require('../../build/process.js')
+var fs = require('fs')
+
 describe('process', function () {
+  describe('Util Test', function () {
+    it('read Mock', function () {
+      assert(
+        fs.readFileSync('./__test__/mock.html', {
+          encoding: 'utf8',
+        }),
+      )
+    })
+  })
+
   describe('processTextNode', function () {
     const textBody = `This is Test`
     it('Only TextNode', function () {
@@ -27,24 +43,27 @@ describe('process', function () {
   })
 
   describe('processElementtNode', function () {
-    it('Only TextNode', function () {
-      const mockASTStack = { tag: { children: [] } }
-      const nextTextNode = processTextNode(textBody, 0, mockASTStack)
-      assert.equal(nextTextNode, -1)
-      assert.deepEqual(mockASTStack.tag.children[0], {
-        type: Type.TEXT,
-        text: 'This is Test',
-      })
-    })
-
-    it('get Next Node', function () {
-      const textAndNodebody = `${textBody}<div>tag</div>`
-      const mockASTStack = { tag: { children: [] } }
-      const nextTextNode = processTextNode(textAndNodebody, 0, mockASTStack)
-      assert.equal(nextTextNode, textBody.length)
-      assert.deepEqual(mockASTStack.tag.children[0], {
-        type: Type.TEXT,
-        text: 'This is Test',
+    it('void Element', function () {
+      const result = {
+        name: 'ROOT',
+        type: Type.NODE,
+        children: [],
+      }
+      const stack = initRootStack(result)
+      const voidElement = `<img />`
+      const endIndex = voidElement.indexOf('>', 0)
+      const currentStack = stack.pop()
+      const isClose = processElementNode(
+        voidElement,
+        endIndex,
+        0,
+        currentStack,
+        stack,
+      )
+      assert.deepEqual(result, {
+        name: 'ROOT',
+        type: 'NODE',
+        children: [{ name: 'img ', type: 'NODE', children: [] }],
       })
     })
   })
