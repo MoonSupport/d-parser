300006843130bb7dab4708dea62626264d1997c5
MoonSupport <jiwon3346@naver.com>
2020-08-04 00:28:57 +0900

    설정 변경

--- .mocharc.js
+++ .mocharc.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-module.exports ={
-    "diff": true,
-    "extension": ["js"],
-    "package": "./package.json",
-    "reporter": "spec",
-    "slow": 75,
-    "timeout": 2000,
-    "ui": "bdd",
-    "watch-files": [ "__test__/**/*.js"],
+module.exports = {
+  diff: true,
+  extension: ['js'],
+  package: './package.json',
+  reporter: 'spec',
+  slow: 75,
+  timeout: 2000,
+  ui: 'bdd',
+  'watch-files': ['__test__/**/*.js'],
 }
